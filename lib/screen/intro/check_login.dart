d5896924708f9eaf365da3f7cfd489feb3ec0f49
hai061898 <truonghaiauto98@gmail.com>
2022-02-04 21:49:36 +0700

    check login page

---
+++ lib/screen/intro/check_login.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CheckLoginScreen extends StatefulWidget {
+  const CheckLoginScreen({ Key? key }) : super(key: key);
+
+  @override
+  _CheckLoginScreenState createState() => _CheckLoginScreenState();
+}
+
+class _CheckLoginScreenState extends State<CheckLoginScreen> {
+  late AnimationController _animationController;
+  
+  late Animation<double> _scaleAnimation;
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
