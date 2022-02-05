fee908544940cc29579844792e23e1c15ef85ac2
hai061898 <truonghaiauto98@gmail.com>
2022-02-05 19:45:51 +0700

    bloc user

--- lib/bloc/user/user_state.dart
+++ lib/bloc/user/user_state.dart
@@ -1,8 +1,32 @@
+// ignore_for_file: prefer_const_constructors_in_immutables
+
 part of 'user_bloc.dart';
 
 @immutable 
-class  UserState {
-  // final User? user;
+class UserState {
+
+  final String pictureProfilePath;
+
+  final int uidAddress;
+  final String addressName;
+  
+  final User? user;
+
+  UserState({
+    this.uidAddress = 0,
+    this.addressName = '',
+    this.pictureProfilePath = '',
+    this.user
+  });
+
+  UserState copyWith({ int? uidAddress, String? addressName, String? pictureProfilePath, User? user })
+    => UserState(
+      uidAddress: uidAddress ?? this.uidAddress,
+      addressName: addressName ?? this.addressName,
+      pictureProfilePath: pictureProfilePath ?? this.pictureProfilePath,
+      user: user ?? this.user
+    );
+
 }
 
 class LoadingUserState extends UserState {}
