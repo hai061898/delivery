cb27af204bf62874c3d715a406a25fffec81203f
hai061898 <truonghaiauto98@gmail.com>
2022-02-10 21:07:18 +0700

    10/2 model product + category

---
+++ lib/models/product_cart.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: unnecessary_this
+
+class ProductCart {
+  String uidProduct;
+  String imageProduct;
+  String nameProduct;
+  double price;
+  int quantity;
+
+  ProductCart(
+      {required this.uidProduct,
+      required this.imageProduct,
+      required this.nameProduct,
+      required this.price,
+      required this.quantity});
+
+  Map<String, dynamic> toJson() {
+    return {
+      "uidProduct": this.uidProduct,
+      "price": this.price,
+      "quantity": this.quantity
+    };
+  }
+}
